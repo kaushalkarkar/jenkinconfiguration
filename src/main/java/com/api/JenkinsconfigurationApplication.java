f3bd30ca519a66989a6aff93b71d9a1b9eeae1ee
Karkar Kaushal <87358709+kaushalkarkar@users.noreply.github.com>
2023-05-01 14:12:30 +0530

    parameter added

--- src/main/java/com/api/JenkinsconfigurationApplication.java
+++ src/main/java/com/api/JenkinsconfigurationApplication.java
@@ -2,6 +2,7 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JenkinsconfigurationApplication {
@@ -10,4 +11,14 @@ public class JenkinsconfigurationApplication {
 		SpringApplication.run(JenkinsconfigurationApplication.class, args);
 	}
 
+	
+	
+	@GetMapping
+	public String user()
+	{
+		return null;
+	
+	}
+	
+	
 }
